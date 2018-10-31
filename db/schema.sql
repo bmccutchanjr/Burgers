98158b8244d01816bfaca9f1af99d3ffc2b5ebfe
Bill McCutchan <bmccutchanjr@gmail.com>
2018-10-31 18:29:16 -0400

    initial code structure

---
+++ db/schema.sql
@@ -0,0 +1,15 @@
+drop database BurgersDB;
+create database if not exists BurgersDB;
+use BurgersDB;
+
+create table Burgers
+(   id              integer         not null
+                                    auto_increment
+                                    not null,
+                            
+    burger_name     varchar(30)     not null,
+    
+    devoured        boolean         default false,
+    
+    primary key (id)
+);
---
+++ db/seeds.sql
@@ -0,0 +1,8 @@
+use BurgersDB;
+
+insert into burgers (burger_name)
+values ("Big Mac"),
+       ("Royale with Cheese"),
+       ("Tofu and Portobello with bacon");
+
+-- select * from burgers;
