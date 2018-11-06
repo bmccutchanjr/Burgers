3252f9327f8949e3ce346c9f662812820b22d9ad
Bill McCutchan <bmccutchanjr@gmail.com>
2018-11-05 21:51:42 -0500

    changes for Heroku deployment

--- db/schema.sql
+++ db/schema.sql
@@ -3,13 +3,20 @@ create database if not exists BurgersDB;
 use BurgersDB;
 
 create table Burgers
-(   id              integer         not null
-                                    auto_increment
-                                    not null,
+(   id              integer
+                    not null
+                    auto_increment
+                    not null,
                             
-    burger_name     varchar(50)     not null,
+    burger_name     varchar(50)
+                    not null,
     
-    devoured        boolean         default false,
+    devoured        boolean
+                    default false,
+
+    createdAt       timestamp
+                    not null
+                    default CURRENT_TIMESTAMP,
     
     primary key (id)
 );
